af51ba4cdf7d10ff5019c538c12139c0c519a25e
VladP97 <vlad.potapov1997@gmail.com>
2017-02-21 00:09:10 +0300

    additional_2/feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function flatten(array) {
-  // your solution
+    var newArr = [];
+    var arr = joinToArr(array, newArr);
+    arr.pop();
+    return arr;
+}
+
+joinToArr = function(array,newArr) {
+    if (array instanceof Array == false) {
+        return [];
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] instanceof Array == true) {
+            newArr.push(arguments.callee(array[i],newArr));
+        } else {
+            newArr.push(array[i]);
+        }
+    }
+    return newArr;
 }
